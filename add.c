b178be203558c56181719f6a9ca2be66c45add78
MBEDOBE <danielbushbaby@gmail.com>
2022-12-15 05:35:39 -0800

    task 4

---
+++ add.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * addop - adds the top two elements together
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void addop(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	(*stack)->next->n += (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
