3aaa32dca99273625c5ab66f8b14718298af610a
MBEDOBE <danielbushbaby@gmail.com>
2022-12-15 05:38:10 -0800

    task 5

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * nop - does literally nothing
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void nop(stack_t **stack, unsigned int nline)
+{
+	(void)stack;
+	(void)nline;
+}
