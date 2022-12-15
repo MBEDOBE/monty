b1aba2c3f541dd5cba58de70277aed697aa93586
MBEDOBE <danielbushbaby@gmail.com>
2022-12-15 05:34:08 -0800

    task 3

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void swap(stack_t **stack, unsigned int nline)
+{
+	int temp;
+
+	if (stack == NULL || *stack == NULL || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
